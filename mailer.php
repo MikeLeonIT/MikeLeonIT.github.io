f718293e7ea289904a5e0d589ad69442ba29d90d
MikeLeonIT <MikeLeonIT@yandex.ru>
2023-11-09 13:21:25 +0300

    fix_mix

--- mailer.php
+++ mailer.php
@@ -1,13 +1,55 @@
-<?php
-require 'PHPMailerAutoload.php';
-$mail = new PHPMailer;
-$mail->setFrom('mikeleonit@yandex.ru', 'Your Name');
-$mail->addAddress('myfriend@example.net', 'My Friend');
-$mail->Subject  = 'First PHPMailer Message';
-$mail->Body     = 'Hi! This is my first e-mail sent through PHPMailer.';
-if(!$mail->send()) {
-  echo 'Message was not sent.';
-  echo 'Mailer error: ' . $mail->ErrorInfo;
-} else {
-  echo 'Message has been sent.';
+<?php 
+//date_default_timezone_set('Etc/UTC');
+require_once 'PHPMailer/PHPMailerAutoload.php';
+if(isset($_POST['submit']) && isset($_POST['email'])&& isset($_POST['message']))
+{ 
+
+    if(!filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+    {
+        //SHOW ERROR MESSAGE
+    }
+    else
+    {
+        $mail = new PHPMailer(true);                          // Passing `true` enables exceptions
+
+        //Server settings
+        $mail->SMTPDebug = 1;                                 // Enable verbose debug output
+        $mail->isSMTP(true);                                  // Set mailer to use SMTP
+        $mail->Host = 'smtp.Gmail.com';                       // Specify main and backup SMTP servers
+        $mail->SMTPAuth = true;                               // Enable SMTP authentication
+        $mail->Username = 'luckynath4@gmail.com';             // SMTP username
+        $mail->Password = 'zedlucky@lucky';                   // SMTP password
+        $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+        $mail->Port = 587;                                    // TCP port to connect to,25,587-tls,465-ssl.
+        //$mail->AddCC($_POST['email'],$_POST['name']);
+        $mail->WordWrap = 50;
+
+        //Recipients
+        $mail->setFrom($_POST['email'],$_POST['name']);
+        $mail->addAddress("mikeleonit@yandex.ru","PROREX");    // Add a recipient
+
+
+        //Content
+        $mail->isHTML(true);                                  // Set email format to HTML
+        $mail->Subject = "PROREX CONTACT";
+        $mail->Body = "<h3>".$_POST['message']."</h3>";
+
+         if( $mail->send())
+         {
+             //echo "email was send";
+             echo '<script language="javascript">';
+             echo 'alert("message successfully sent")';
+             echo '</script>';
+             header("Refresh:3; url=index.php");
+
+             exit();
+         }
+         else
+         {
+             echo 'Mailer error: ' . $mail->ErrorInfo;
+         }   
+
+
+    }   
 }
+?>   

a3679be3d95e1ef047edcf2f2f91dbe54ad6bd2f
Mikhail Leonov <MikeLeonIt@yandex.ru>
2023-12-04 16:31:54 +0300

    ads

--- test_ui.py
+++ test_ui.py
@@ -1,2 +1,17 @@
-def start_ui():
-    return 'asd'
+import asyncio
+import time
+async def start_ui():
+    from pytube import YouTube
+    import ssl
+    import io
+    # from js import document, FileReader, Uint8Array
+
+    # YouTube('https://youtu.be/FeOkmOWeC4k').streams.filter(mime_type='video/mp4', res='720p').first().download(max_retries=500)
+
+    try:
+        f = await YouTube('https://youtu.be/FeOkmOWeC4k').streams.filter(mime_type='video/mp4', res='720p').first().download(max_retries=500)
+        time.sleep(200)
+        await f
+
+    except Exception as exc:
+        print(exc)

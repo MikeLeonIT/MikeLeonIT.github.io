83f22920d643ecd969894da243d5d8bde360b539
Mikhail Leonov <MikeLeonIt@yandex.ru>
2023-12-15 08:05:58 +0300

    asd

--- mailer.php
+++ mailer.php
@@ -1,55 +1,33 @@
-<?php 
-//date_default_timezone_set('Etc/UTC');
-require_once 'PHPMailer/PHPMailerAutoload.php';
-if(isset($_POST['submit']) && isset($_POST['email'])&& isset($_POST['message']))
-{ 
+<?php
+@include_once("Mail.php");
 
-    if(!filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
-    {
-        //SHOW ERROR MESSAGE
-    }
-    else
-    {
-        $mail = new PHPMailer(true);                          // Passing `true` enables exceptions
+$name = $_POST["Name"];
+$message = $_POST["Message"];
+$from = $_POST["Email"];
 
-        //Server settings
-        $mail->SMTPDebug = 1;                                 // Enable verbose debug output
-        $mail->isSMTP(true);                                  // Set mailer to use SMTP
-        $mail->Host = 'smtp.Gmail.com';                       // Specify main and backup SMTP servers
-        $mail->SMTPAuth = true;                               // Enable SMTP authentication
-        $mail->Username = 'luckynath4@gmail.com';             // SMTP username
-        $mail->Password = 'zedlucky@lucky';                   // SMTP password
-        $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
-        $mail->Port = 587;                                    // TCP port to connect to,25,587-tls,465-ssl.
-        //$mail->AddCC($_POST['email'],$_POST['name']);
-        $mail->WordWrap = 50;
+// remove the backslashes that normally appears when entering " or '
+$name = stripslashes($name);
+$message = stripslashes($message);
+$from = stripslashes($from);
 
-        //Recipients
-        $mail->setFrom($_POST['email'],$_POST['name']);
-        $mail->addAddress("mikeleonit@yandex.ru","PROREX");    // Add a recipient
+$to_address = "mikeleonit@yandex.ru";
 
-
-        //Content
-        $mail->isHTML(true);                                  // Set email format to HTML
-        $mail->Subject = "PROREX CONTACT";
-        $mail->Body = "<h3>".$_POST['message']."</h3>";
-
-         if( $mail->send())
-         {
-             //echo "email was send";
-             echo '<script language="javascript">';
-             echo 'alert("message successfully sent")';
-             echo '</script>';
-             header("Refresh:3; url=index.php");
-
-             exit();
-         }
-         else
-         {
-             echo 'Mailer error: ' . $mail->ErrorInfo;
-         }   
-
-
-    }   
+// Check to see if a message was recieved
+if(isset($message) and $message!="") {
+    // send the message and show this page
+    mail($to_address, $_SERVER['REMOTE_ADDR']."\n\nName: ".$name."\n\n".$message, "From: $from");
+} else {
+    echo "no variables received, this page cannot be accessed directly";
+    exit;
 }
-?>   
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Contact Form - Thank you</title>
+    </head>
+    <body>
+        <p>Thank you for your submission.</p>
+    </body>
+</html>
